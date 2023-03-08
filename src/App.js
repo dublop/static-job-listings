8daaf9935ae4a55e12de5c86fa462518affd94ac
dublop <dnlopezavila@gmail.com>
2023-03-08 11:12:33 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div>
+        <img />
+        <p>Photosnap</p>
+        <p>New!</p>
+        <p>Featured</p>
+        <p>Senior Frontend Developer</p>
+        <span>1d ago</span>
+        <span>Full Time</span>
+        <span>USA only</span>
+        <div>
+          <a href="">Frontend</a>
+          <a href="">Senior</a>
+          <a href="">HTML</a>
+          <a href="">CSS</a>
+          <a href="">JavaScript</a>       
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
